407efcd061f8c3965b73ae44da4988f585ae35a8
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-01-29 01:13:44 -0600

    added true/flase prompt to questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,19 +1,34 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of the project?'
+        message: 'What is the title of the project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true
+            } else {
+                console.log('Please provide title for your README.md.')
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'description',
-        message: 'Give a description of the project'
+        message: 'Give a description of the project',
+        validate: descriptionInput => {
+            if (descriptionInput){
+                return true 
+            } else {
+                console.log('Please provide a description for the project')
+            }
+        }
     },
     {
        type: 'list',
@@ -29,40 +44,92 @@ const questions = [
     {
         type: ' input',
         name: 'installation',
-        message: 'What is the installation process?'
+        message: 'What is the installation process?',
+        validate: installationInput => {
+            if (installationInput){
+                return true
+            } else {
+                console.log('Plese provide installations used')
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'What is the usage of this project?'
+        message: 'What is the usage of this project?',
+        validate: usageInput => {
+            if (usageInput){
+                return true
+            } else {
+                console.log('Plese enter the project usage')
+                return false;
+            }
+        }
     },
     {
         type: 'list',
         name: 'license',
         message: 'Choose a license for this project:',
         choice: ['Apache', 'Boost', 'GNU', 'IBM', 'MIT', 'Mozilla', 'Open Database', 'None']
+    
     },
     {
         type: 'input',
         name: 'contributions',
-        message: 'What are the guidelins for contribution to this project?'
+        message: 'What are the guidelins for contribution to this project?',
+        validate: contributionsInput => {
+            if (contributionsInput) {
+                return true
+            } else {
+                console.log('Please enter contribution guidelines.')
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'test',
-        message: 'what are the test instructions?'
+        message: 'what are the test instructions?',
+        validate: testInput => {
+            if (testInput){
+                return true
+            } else {
+                console.log('Provide test intructions')
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'username',
-        message: 'what is your Github username?'
+        message: 'what is your Github username?',
+        validate: usernameInput => {
+            if (usernameInput){
+                return true;
+            } else {
+                console.log('Please provide a valid Github username.')
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'email',
-        message: 'What is your email addess?'
+        message: 'What is your email address?',
+        validate: usernameInput => {
+            if (usernameInput){
+                return true;
+            } else {
+                console.log('Please provide a valid email address.')
+                return false;
+            }
+        }
     },
 ];
+ const promptUser = () => {
+     return inquirer.prompt(questions);
+ }
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
