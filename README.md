242d5a4ab5f1c6ed90af6234d3952a69b47b5a5f
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-01-30 18:59:23 -0600

    applied license badges

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# test
-testing
+# Portfolio-generator
+
+  ## Description
+this project will generate a portfolio per users command
+
+## Table of Contents
+-[Description](#description)
+-[Installation](#installation)
+-[Language](#language)
+-[Usage](#usage)
+-[License](#license)
+-[Contributions](#contributions)
+-[Test](#test)
+-[Questions](#questions)
+
+## Installation
+inquirer
+
+## Language
+JavaScript,HTML,ES6,jQuery,Node
+
+## Usage
+generate profile quicker for user
+
+## License
+undefined
+
+## Contributions
+no additional contributer
+
+## Test
+no test
+
+## Questions
+find me on Github: [samismail2010](https://github.com/samismail2010)
+Email me with any questions: [samismail2010@gmail.com](mailto:samismail2010@gmail.com)
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,26 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (data.license == 'Apache'){
+    //if this license is selected then we want this badge icon to return to us
+      return licensebadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-yellowgreen.svg)]'
+    } else if (data.license == 'Boost') {
+      return licensebadge = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]'
+    } else if (data.license == 'GNU') {
+      return licensebadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]'
+    } else if (data.license == 'IBM') {
+      return licensebadge = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)]'
+    } else if (data.license == 'MIT') {
+      return licensebadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
+    } else if (data.license == 'Mozilla') {
+      return licensebadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]'
+    } else if (data.license == 'Open Database') {
+      return licensebadge = '[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)]'
+    }
+    else {
+      return licensebadge = '';
+    }
+  }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +33,8 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data)}${renderLicenseLink}
 
   ## Description
 ${data.description}
