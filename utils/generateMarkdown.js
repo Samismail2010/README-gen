b6ece11b7f8150089399b87d4487242be821bb71
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-01-30 22:56:44 -0600

    applied license links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
+function renderLicenseBadge(data) {
   if (data.license == 'Apache'){
     //if this license is selected then we want this badge icon to return to us
       return licensebadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-yellowgreen.svg)]'
@@ -24,7 +24,25 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {
+  if (data.license == 'Apache'){
+    return licenselink = '(https://opensource.org/licenses/Apache-2.0)'
+  } else if (data.license == 'Boost') {
+    return licenselink = '(https://www.boost.org/LICENSE_1_0.txt)'
+  } else if (data.license == 'GNU'){
+    return licenselink = '(https://www.gnu.org/licenses/gpl-3.0)'
+  } else if (data.license == 'IBM'){
+    return licenselink = '(https://opensource.org/licenses/IPL-1.0)'
+  } else if (data.license = 'MIT'){
+    return licenselink = '(https://opensource.org/licenses/MIT)'
+  } else if (data.license = 'Mozilla'){
+    return licenselink = '(https://opensource.org/licenses/MPL-2.0)'
+  } else if (data.license = 'Open Database'){
+    return licenselink = '(https://opendatacommons.org/licenses/odbl/)'
+  } else {
+    return licenselink = '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
